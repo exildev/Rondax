d410cbef6e52f20bf14d4387781e32fd4f12805c
antaresgeo <ctrujillozt@gmail.com>
2016-09-02 18:44:42 -0500

    Usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from operacion import models as operacion
 
+
+class Operario(User):
+    turno = models.ForeignKey(operacion.Turno)
+# end class
 # Create your models here.
