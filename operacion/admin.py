f5a9e2a9442000bc18d46bab0a97be703fb38cbe
luismoralesp <luismiguel.mopa@gmail.com>
2016-09-02 21:03:19 -0500

    dynamic form

--- operacion/admin.py
+++ operacion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import models
 
-# Register your models here.
+admin.site.register(models.Empresa)
+admin.site.register(models.Ciudad)
+admin.site.register(models.Planta)
+admin.site.register(models.Unidad)
+admin.site.register(models.Turno)
+admin.site.register(models.Equipo)
+admin.site.register(models.Medida)
+admin.site.register(models.Variable)
